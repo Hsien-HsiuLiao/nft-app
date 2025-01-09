29210d0f638f4bb771e321ef77265cc3bed72c0a
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-01-09 06:09:32 -0800

    add infura url to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-waffle');
 
+const INFURA_URL = 'https://sepolia.infura.io/v3/e8746dd884324d37984f917e061064ee';
+
 module.exports = {
   solidity: "0.8.27",
 };
