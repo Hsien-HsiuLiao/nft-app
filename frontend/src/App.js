8970a520fad89fb69beffdd4845463e10eeeac19
hsien <hsien_hsiu.liao@yahoo.com>
2025-02-04 23:05:56 -0800

    add handleClick for button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,22 +8,34 @@ function App() {
   /* useEffect( () => {
     const init = async () => {
       const { nft } = await getBlockchain();
-      const tokenURI = await nft.tokenURI(0);
-      const { data } = await axios.get(tokenURI);
+      const tokenURI = await nft.tokenURI(0); //tokenId = 0, nft.tokenURI(tokenId)
+      const { data } = await axios.get(tokenURI); //https://gateway.pinata.cloud/ipfs/bafkreid43ipihuvs4a2gu46ekp4cqjlq3wee52vfp5sj2jbxqgcoj2tfem/
      // console.log(data.result.image);
       setTokenInfo(data.result);
     };
     init();
   }, []); */
 
+  const handleClick = async () => {
+    console.log('Button clicked!');
+    const { nft } = await getBlockchain();
+    const tokenURI = "https://gateway.pinata.cloud/ipfs/bafkreid43ipihuvs4a2gu46ekp4cqjlq3wee52vfp5sj2jbxqgcoj2tfem/"
+    const { data } = await axios.get(tokenURI);
+    console.log(data[0]);
+    setTokenInfo(data[0]);
+    console.log(tokenInfo);
+  };
+
+  
+
   if (typeof tokenInfo === 'undefined') {
     return (
       <div>
-      <h3>Getting token info...</h3>
-      <p>install MetaMask <a href="https://metamask.io/"> https://metamask.io/</a></p>
-      <button>Connect MetaMask</button>
-      <p>Connect Metamask to the testnet to display the NFT</p>
-      <p>Get test eth sepolia <a href="https://www.alchemy.com/faucets/ethereum-sepolia">faucet</a></p>
+        <h3>Getting token info...</h3>
+        <p>install MetaMask <a href="https://metamask.io/"> https://metamask.io/</a></p>
+        <button onClick={handleClick}>Connect MetaMask</button>
+        <p>Connect Metamask to the testnet to display the NFT</p>
+        <p>Get test eth sepolia <a href="https://www.alchemy.com/faucets/ethereum-sepolia">faucet</a></p>
       </div>
     );
   }
